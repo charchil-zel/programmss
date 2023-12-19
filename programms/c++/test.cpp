8ec0cf36ab3a7208e0812b2f58cc4b0261732e9c
charchil-zel <churchil.neupane@gmail.com>
2023-12-19 15:24:13 +0545

    modified

---
+++ programms/c++/test.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main() {
+  std::cout << "my name is charchil Neupane" << std::endl;
+  return 0;
+}
--- programms/web/prac.js
+++ programms/web/prac.js
@@ -1,21 +1,20 @@
 
-function factors()
+
+function factors(no)
 {
-    let no=prompt("No. to display Factor: ");
     for (var i=1;i<=no;i++)
     {
         if (no%i==0)
-        {
+        
             document.write(i+ " ");
-        }
+        
     }
 }
-
-factors();
+let no=prompt("No. to display Factor: ");
+factors(no);
 /*
-function factorial()
+function factorial(no)
 {
-    let no=prompt("Number: ");
   
     var a=1;
     for (var i=1;i<=no;i++)
@@ -24,11 +23,11 @@ function factorial()
     }
    document.write(a);
 }
-factorial();
+let no=prompt("Number: ");
+factorial(no);
 
-function fibonacci()
+function fibonacci(no)
 {
-    let no=prompt("No. of terms for the series: ");
     var a=0;
     var b=1;
     for (var i=1;i<=no;i++)
@@ -39,11 +38,11 @@ function fibonacci()
         b=c;
     }
 }
-fibonacci();
+let no=prompt("No. of terms for the series: ");
+fibonacci(no);
 
-function palindrome()
+function palindrome(no)
 {
-    let no=prompt("Number to check the palindrome: ");
    var rev=0,rem,no1;
     no1=no;
     while (no!=0)
@@ -53,30 +52,39 @@ function palindrome()
         no=parseInt(no/10);
    }
 
-    (no1==rev)?document.write("palindrome"):document.write("not palindrome");
+    return (no1==rev)?true:false;
 }
-
-palindrome();
-function armstrong()
+let no=prompt("Number to check the palindrome: ");
+(palindrome(no)==true)?document.write("yes palindrome"):document.write("no palindrome");
+/*
+function armstrong(no)
 {
-    let no,no1, rem,res=0,digit=0;
-    no=prompt("Number to check Armstrong: ");
-    
+    let no1, rem,res=0,digit=0;
     no1=no;
     while (no!=0)
     {
         no=parseInt(no/10);
         digit++;
     }
-
-   no=no1;
+    no=no1;
     while (no1!=0)
     {
         rem=no1%10;
         res+=rem**digit;
         no1=parseInt(no1/10);
     }
-    (res==no)?document.write("yes armstrong"):document.write("not armstrong");
+    return (res==no)?true:false;
 }
+    let no=prompt("Number to check Armstrong: ");
 
-armstrong();*/
+    if (armstrong(no))
+    
+        document.write("yes armstrong");
+    
+    else
+    
+        document.write("no armstrong"); 
+    */
+
+
+
